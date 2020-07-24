9c4828f7487cd69f2ab8c80b50ce21afd327c9da
ez <azozsahb99@gmail.com>
2020-07-24 23:13:03 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/ezaldeen/search_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ezaldeen.search_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,246 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+List<String> allNames = ["ahmed", "ali", "john", "user"];
+var mainColor = Color(0xff1B3954);
+var textColor = Color(0xff727272);
+var accentColor = Color(0xff16ADE1);
+var whiteText = Color(0xffF5F5F5);
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new _SearchState(),
+    );
+  }
+}
+
+class _SearchState extends StatelessWidget {
+  String _sortValue;
+  String _ascValue = "ASC";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            forceElevated: true,
+            elevation: 4,
+            floating: true,
+            snap: true,
+            title: Text(
+              "Search App",
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.search,
+                ),
+                onPressed: () {
+                  showSearch(
+                    context: context,
+                    delegate: CustomSearchDelegate(),
+                  );
+                },
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.filter_list,
+                ),
+                onPressed: () {
+                  showFilterDialog(context);
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> showFilterDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext build) {
+          return StatefulBuilder(builder: (context, setState) {
+            return AlertDialog(
+              title: Center(
+                  child: Text(
+                "Filter",
+                style: TextStyle(color: mainColor),
+              )),
+              content: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(top: 12, right: 10),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(right: 16.0),
+                            child: Icon(
+                              Icons.sort,
+                              color: Color(0xff808080),
+                            ),
+                          ),
+                          Expanded(
+                            child: DropdownButtonHideUnderline(
+                              child: DropdownButton<String>(
+                                isExpanded: true,
+                                hint: Text("Sort by"),
+                                items: <String>[
+                                  "Name",
+                                  "Age",
+                                  "Date",
+                                ].map((String value) {
+                                  return DropdownMenuItem(
+                                    value: value,
+                                    child: Text(value,
+                                        style: TextStyle(
+                                            color: textColor, fontSize: 16)),
+                                  );
+                                }).toList(),
+                                value: _sortValue,
+                                onChanged: (newValue) {
+                                  setState(() {
+                                    _sortValue = newValue;
+                                  });
+                                },
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 8, right: 10),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(right: 16.0),
+                            child: Icon(
+                              Icons.sort_by_alpha,
+                              color: Color(0xff808080),
+                            ),
+                          ),
+                          Expanded(
+                            child: DropdownButtonHideUnderline(
+                              child: DropdownButton<String>(
+                                isExpanded: true,
+                                items: <String>[
+                                  "ASC",
+                                  "DESC",
+                                ].map((String value) {
+                                  return DropdownMenuItem(
+                                    value: value,
+                                    child: Text(value,
+                                        style: TextStyle(
+                                            color: textColor, fontSize: 16)),
+                                  );
+                                }).toList(),
+                                value: _ascValue,
+                                onChanged: (newValue) {
+                                  setState(() {
+                                    _ascValue = newValue;
+                                  });
+                                },
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          });
+        });
+  }
+}
+
+class CustomSearchDelegate extends SearchDelegate {
+  var suggestion = ["ahmed", "ali", "mohammad"];
+  List<String> searchResult = List();
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    searchResult.clear();
+    searchResult =
+        allNames.where((element) => element.startsWith(query)).toList();
+    return Container(
+      margin: EdgeInsets.all(20),
+      child: ListView(
+          padding: EdgeInsets.only(top: 8, bottom: 8),
+          scrollDirection: Axis.vertical,
+          children: List.generate(suggestion.length, (index) {
+            var item = suggestion[index];
+            return Card(
+              color: Colors.white,
+              child: Container(padding: EdgeInsets.all(16), child: Text(item)),
+            );
+          })),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // This method is called everytime the search term changes.
+    // If you want to add search suggestions as the user enters their search term, this is the place to do that.
+    final suggestionList = query.isEmpty
+        ? suggestion
+        : allNames.where((element) => element.startsWith(query)).toList();
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        onTap: () {
+          if (query.isEmpty) {
+            query = suggestion[index];
+          }
+        },
+        leading: Icon(query.isEmpty ? Icons.history : Icons.search),
+        title: RichText(
+            text: TextSpan(
+                text: suggestionList[index].substring(0, query.length),
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                children: [
+              TextSpan(
+                text: suggestionList[index].substring(query.length),
+                style: TextStyle(color: textColor),
+              )
+            ])),
+      ),
+      itemCount: suggestionList.length,
+    );
+  }
+}
